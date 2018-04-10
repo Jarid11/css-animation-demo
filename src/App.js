1aaa402b2c74747c42a870aed9dffc9fb2b94500
unknown <jinyeob.kim.0818@gmail.com>
2018-04-10 09:56:25 -0500

    finished

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="container">
+          <div className="first bar" />
+          <div className="second bar" />
+          <div className="third bar" />
+          <div className="fourth bar" />
+          <div className="fifth bar" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
